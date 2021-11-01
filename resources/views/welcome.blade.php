e37c8e1819b7fa9d800ae3e93dc4eeedcea9bbbf
Sanoks <468282+cbraddoss@users.noreply.github.com>
2021-10-31 19:42:08 -0500

    update welcome.blade.php
    update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,11 +30,11 @@
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
-                        
                     </div>
                 </div>
 
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                        <p><a href="https://github.com/cbraddoss">https://github.com/cbraddoss</a></p>
                     
                 </div>
             </div>
